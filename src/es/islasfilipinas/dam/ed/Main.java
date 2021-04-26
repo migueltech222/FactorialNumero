c257154275f8cf403edef831ce51fdf2e80311ec
Miguel <migueltech222@gmail.com>
2021-04-26 19:31:34 +0200

    Version inicial del programa

--- src/es/islasfilipinas/dam/ed/Main.java
+++ src/es/islasfilipinas/dam/ed/Main.java
@@ -1,10 +1,24 @@
 package es.islasfilipinas.dam.ed;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		int num = 0;
+		Scanner entrada = new Scanner(System.in);
+		
+		System.out.println("Programa para saber el factorial de un número\n");
+		System.out.println("Introduzca el número del cual quieres su factorial: ");
+		num = entrada.nextInt();
+		
+		for (int i=num-1; i>1; i--) {
+			num = num*i;
+		}
+		
+		System.out.println("El factorial del número introducido es: "+num);
+		
 	}
 
 }
